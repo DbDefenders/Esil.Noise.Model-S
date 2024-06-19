97c4d13d527aab8bafe3142818e6a5cdeed9b9a3
Viyyy <1793707472@qq.com>
2024-06-19 17:24:59 +0800

    add Birdclef source

--- datasets/models/sources.py
+++ datasets/models/sources.py
@@ -7,8 +7,9 @@ from .base import DataSourceBase
 class ESC50DataSource(DataSourceBase):
     def __init__(
         self,
-        meta_file: str,
         base_dir: str,
+        meta_file: str = "static/meta_file/esc50.csv",
+        dataframe: pd.DataFrame = None,
         name: str = "ESC50",
         label: int = None,
         length: int = None,
@@ -16,7 +17,10 @@ class ESC50DataSource(DataSourceBase):
         self.meta_file = meta_file
 
         childs = None
-        annotations = pd.read_csv(meta_file)
+        if dataframe is None:
+            dataframe = pd.read_csv(meta_file)
+        self.__dataframe__ = dataframe
+        annotations = self.__dataframe__
         annotations["target"] = annotations["target"].astype(int)
         annotations = (
             annotations[annotations["target"] == label]
@@ -33,6 +37,7 @@ class ESC50DataSource(DataSourceBase):
                     ESC50DataSource(
                         meta_file=meta_file,
                         base_dir=base_dir,
+                        dataframe=self.__dataframe__,
                         name=tmp.iloc[0]["category"],
                         label=t,
                         length=len(tmp),
@@ -44,7 +49,7 @@ class ESC50DataSource(DataSourceBase):
         )
 
     def get_file_path(self, index: int) -> str:
-        df = pd.read_csv(self.meta_file)
+        df = self.__dataframe__
         ret = df[df["target"] == self.id].reindex().iloc[index]
         return os.path.join(f'fold{ret["fold"]}', ret["filename"])
 
@@ -56,8 +61,9 @@ class ESC50DataSource(DataSourceBase):
 class US8KDataSource(DataSourceBase):
     def __init__(
         self,
-        meta_file: str,
         base_dir: str,
+        meta_file: str = "static/meta_file/UrbanSound8K.csv",
+        dataframe: pd.DataFrame = None,
         name: str = "US8K",
         label: int = None,
         length: int = None,
@@ -65,7 +71,10 @@ class US8KDataSource(DataSourceBase):
         self.meta_file = meta_file
 
         childs = None
-        annotations = pd.read_csv(meta_file)
+        if dataframe is None:
+            dataframe = pd.read_csv(meta_file)
+        self.__dataframe__ = dataframe
+        annotations = self.__dataframe__
         annotations["classID"] = annotations["classID"].astype(int)
         annotations = (
             annotations[annotations["classID"] == label]
@@ -82,6 +91,7 @@ class US8KDataSource(DataSourceBase):
                     US8KDataSource(
                         base_dir=base_dir,
                         meta_file=meta_file,
+                        dataframe=self.__dataframe__,
                         name=tmp.iloc[0]["class"],
                         length=len(tmp),
                         label=t,
@@ -93,7 +103,7 @@ class US8KDataSource(DataSourceBase):
         )
 
     def get_file_path(self, index: int) -> str:
-        df = pd.read_csv(self.meta_file)
+        df = self.__dataframe__
         ret = df[df["classID"] == self.id].reindex().iloc[index]
         return os.path.join(f'fold{ret["fold"]}', ret["slice_file_name"])
 
@@ -106,7 +116,7 @@ class ProvinceDataSource(DataSourceBase):
     def __init__(
         self,
         base_dir: str,
-        meta_file: str,
+        meta_file: str = "static/meta_file/province.json",
         *,
         name: str,
         label: int = None,
@@ -142,3 +152,60 @@ class ProvinceDataSource(DataSourceBase):
     def __repr__(self):
         properties = ["base_dir", "name", "label", "length", "childs", "parent"]
         return create_repr_str(self, properties)
+    
+class BirdclefDataSource(DataSourceBase):
+    def __init__(
+        self,
+        base_dir: str,  # 数据的基目录        
+        meta_file: str = "static/meta_file/Birdclef.csv",  # 元数据文件的路径
+        name: str = "Birdclef",  # 数据源的名称
+        dataframe: pd.DataFrame = None,  # 元数据文件的内容，可以直接提供，也可以通过meta_file参数读取
+        label: int = None,  # 用于过滤的标签（类别ID）
+        length: int = None,  # 数据源中的数据点数量
+    ):
+        self.meta_file = meta_file  # 将meta_file参数赋值给实例变量
+
+        childs = None  # 初始化childs变量，用于存储子数据源
+        if dataframe is None:
+            dataframe = pd.read_csv(self.meta_file)
+        self.__dataframe__ = dataframe
+        annotations = pd.read_csv(self.meta_file)  # 读取元数据文件
+        annotations["id"] = annotations["id"].astype(int)  # 将id列的数据类型设置为int
+        annotations = (  # 根据label过滤注释，如果没有提供label，则使用所有注释
+            annotations[annotations["id"] == label]
+            if label is not None
+            else annotations
+        )
+        classes = annotations["id"].unique()  # 获取所有唯一的类别ID
+        if len(classes) > 1:  # 如果有多个类别
+            classes.sort()  # 对类别进行排序
+            childs = []  # 初始化childs列表
+            for t in classes:  # 遍历每个类别
+                tmp = annotations[annotations["id"] == t]  # 过滤出当前类别的注释
+                childs.append(  # 将新的数据源添加到childs列表
+                    BirdclefDataSource(
+                        base_dir=base_dir,  # 基目录
+                        meta_file=meta_file,  # 元数据文件
+                        dataframe=self.__dataframe__,
+                        name=tmp.iloc[0]["primary_label"],  # 使用类别的名称
+                        length=len(tmp),  # 使用当前类别的数据点数量
+                        label=t,  # 使用当前类别的标签
+                    )
+                )
+
+        super().__init__(  # 调用基类的初始化方法
+            base_dir=base_dir,  # 基目录
+            name=name,  # 数据源名称
+            label=label,  # 标签
+            length=length,  # 数据点数量
+            childs=childs  # 子数据源
+        )
+
+    def get_file_path(self, index: int) -> str:  # 定义一个方法来获取文件路径
+        df = self.__dataframe__
+        ret = df[df["id"] == self.id].reindex().iloc[index]
+        return os.path.join(f'train_audio', ret["filename"]) # 构建并返回文件路径
+
+    def __repr__(self):  # 定义对象的字符串表示
+        properties = ["meta_file", "base_dir", "name", "label", "length", "childs"]  # 要包含在表示中的属性列表
+        return create_repr_str(self, properties)  # 调用一个辅助函数来创建字符串表示

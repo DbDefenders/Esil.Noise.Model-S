eef4f2063514080ca21d936db519d7e1d90dee9a
Viyyy <1793707472@qq.com>
2024-06-19 17:24:29 +0800

    add wandb transformation func

--- utils/wlog/__init__.py
+++ utils/wlog/__init__.py
@@ -3,27 +3,33 @@
 # Created on: 2024-06-11 10:55:28
 # Description: This file contains the implementation of the WandbLogger class.
 
+from ..plot import plt2ndarray
 
 import wandb
-from  enum import Enum
+from matplotlib.figure import Figure
+from numpy import ndarray
+import pandas as pd
 
-class LogFileType(Enum):
-    Image = 1
-    Audio = 2
-    Table = 3
-        
-class WandbLogger():
-    def __init__(self, *args):
-        self.init_wandb(*args)
-        pass
+def plt2image(fig: Figure) -> wandb.Image:
     
-    def init_wandb(*args):
-        pass
+    img:ndarray = plt2ndarray(fig)
+    
+    if img.size[-1] == 4:
+        mode = "RGBA"
+    elif img.size[-1] == 3:
+        mode = "RGB"
+    else:
+        raise ValueError("Invalid image format")
+    
+    return wandb.Image(img, mode=mode)
 
-    def log_wandb(*args):
-        pass
-    def log_files(file_type:LogFileType, *args):
-        pass
-
-    def finish_wandb(*args):
-        pass
+def df2table(df: pd.DataFrame) -> wandb.Table:
+    # 创建一个 wandb.Table 对象，并指定表头
+    columns = list(df.columns)
+    tb = wandb.Table(columns=columns)
+    
+    # 将 DataFrame 的每一行数据添加到 wandb.Table 中
+    for i, row in df.iterrows():
+        tb.add_data(*row.tolist())
+    
+    return tb
